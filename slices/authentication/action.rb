33cd03f366e0b98f3a7a590757a0e904163e9243
Jovan <jovansr@protonmail.com>
2023-08-04 21:24:04 +0200

    Delete slices and use common actions from app dir

--- slices/authentication/action.rb
+++
@@ -1,7 +0,0 @@
-# auto_register: false
-# frozen_string_literal: true
-
-module Authentication
-  class Action < SocialVibeTracker::Action
-  end
-end
--- spec/slices/authentication/action_spec.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Authentication::Action do
-  xit "works"
-end
