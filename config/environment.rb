96c89dfe2a03fe38d841f7699ea51f69efdc6ce3
ck0jo <jovansr@protonmail.com>
2023-08-27 11:36:45 +0200

    Add some configuration code to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 Hanami.configure do
-  autoload_paths << 'social_vibe_tracker/contracts'
+  case Hanami.env
+  when 'test'
+    Hanami::Model.configure do
+      url = ENV['DATABASE_URL']
+      adapter type: :sql, uri: url if url
+    end
+  end
 end
