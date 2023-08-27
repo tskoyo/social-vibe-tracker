4cf8f8c1706ca36788b48465f9f4d5365d2afafd
ck0jo <jovansr@protonmail.com>
2023-08-27 11:50:43 +0200

    Create Postgres DB in the action

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 Hanami.configure do
-  case Hanami.env
-  when 'test'
-    Hanami::Model.configure do
-      url = ENV['DATABASE_URL']
-      adapter type: :sql, uri: url if url
-    end
-  end
 end
