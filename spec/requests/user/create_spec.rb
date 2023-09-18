2baedc27f80b0f85d658e6e0ac4d05707a54ac8f
Jovan Sremacki <43699919+jovan-sremacki@users.noreply.github.com>
2023-09-18 18:15:27 +0200

    DEV-14-write-request-tests-for-user-creation (#14)
    * [DEV-14] Test request process for user creation
    
    * [DEV-14] Use different email for the testing user
    
    ---------
    
    Co-authored-by: ck0jo <jovansr@protonmail.com>

--- spec/requests/user/create_spec.rb
+++ spec/requests/user/create_spec.rb
@@ -1,18 +1,44 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# RSpec.describe "POST /user/create", type: :request do
-#   let(:expected_response) do
-#     {
-#     name: 'John Doe',
-#     email: 'johndoe@test.com'
-#     }
-#     end
-#   it 'should return response with user data' do
-#     post 'user/create'
+RSpec.describe 'POST /user/create', type: :request do
+  let(:email) { 'johndoe1@test.com' }
+  let(:first_name) { 'John' }
+  let(:last_name) { 'Doe' }
+  let(:password) { 'pa44$w0rD' }
+  let(:password_confirmation) { 'pa44$w0rD' }
+  
+  let(:params) do
+    { user: { email:, first_name:, last_name:, password:, password_confirmation: } }
+  end
 
-#     expect(last_response).to be_successful
-#     expect(last_response.content_type).to eq("application/json; charset=utf-8")
+  let(:request_headers) do
+    {'HTTP_ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json'}
+  end
+  
+  let(:expected_response) do
+    {
+      name: 'John Doe',
+      email: 'johndoe@test.com'
+    }
+  end
 
-#     response_body = JSON.parse(last_response.body)
-#   end
-# end
+  describe 'user creation' do
+    context 'invalid params' do
+      let(:email) { 'johndoe' }
+
+      it 'returns 422 unprocessable' do
+        post '/user/create', params.to_json, request_headers
+
+        expect(last_response).to be_unprocessable
+      end
+    end
+
+    context 'valid params' do
+      it 'creates a user' do
+        post '/user/create', params.to_json, request_headers
+
+        expect(last_response).to be_successful
+      end
+    end
+  end
+end
